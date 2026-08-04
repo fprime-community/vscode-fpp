1cf44e2c33feaededbae4fd805c9451ac569e566
Andrei Tumbar <andrei.tumbar@jpl.nasa.gov>
2026-08-04 07:10:09 -0700

    Fix doc tests

--- fpp_macros/src/lib.rs
+++ fpp_macros/src/lib.rs
@@ -74,7 +74,7 @@ pub fn ast(_attrs: TokenStream, input: TokenStream) -> TokenStream {
 /// 3. If enum, all variants also derive from AstAnnotated
 ///
 /// For structs
-/// ```
+/// ```ignore
 /// #[ast]
 /// #[derive(AstAnnotated)]
 /// pub struct SpecStateMachineInstance {
@@ -86,7 +86,7 @@ pub fn ast(_attrs: TokenStream, input: TokenStream) -> TokenStream {
 /// ```
 ///
 /// For enums:
-/// ```
+/// ```ignore
 /// use fpp_macros::ast_node;
 ///
 /// #[ast]
